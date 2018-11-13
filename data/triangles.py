788815091b97fecafb7bbddff0fb88dc04166c87
Florian Diegruber <florian@diegruber.net>
2018-11-13 14:49:43 +0100

    Created triangles module

---
+++ data/triangles.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 13 14:47:10 2018
+
+@author: flo
+"""
+
+#%%
+
+triangle_definitions = [{"base": 10, "height":2},{"base": 10, "height":3},{"base": 5, "height":2},
+                        {"base": 2, "height":2},{"base": 1, "height":1},{"base": 3, "height":2},
+                        {"base": 10, "height":1},{"base": 10, "height":10},{"base": 3, "height":3},
+                        {"base": 3, "height":4}]
+
+
+#%%
+
+
