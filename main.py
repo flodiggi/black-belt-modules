fb07a5f518c4fc6b39b755d9da16991f4288c260
Florian Diegruber <florian@diegruber.net>
2018-11-13 15:00:46 +0100

    Created Main.py file

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 13 14:50:18 2018
+
+@author: flo
+"""
+
+#%%
+import utils.functions as uf
+import data.triangles as dt
+
+#%%
